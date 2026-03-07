c0986d7dc65344f4b6ea14edf854f5e2922cb1d9
beili <beilian.alvarenga@gmail.com>
2026-03-07 17:39:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -313,4 +313,8 @@ wss.on('connection', ws => {
   });
 });
 
-server.listen(PORT, () => console.log(`Vyntra running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
